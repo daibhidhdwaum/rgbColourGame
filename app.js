21d530a578d69931de70a5070c475bf2f403ee82
David Downie <daviddownie@gmail.com>
2020-08-06 16:02:44 -0400

    Randomly generating the numbers that will make up each colour

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("hello");
+const rgbGame = {};
+
+// grab elements from the dom
+
+// initialize array to store colors
+rgbGame.colourArray = [];
+
+// function to determine colours
+rgbGame.colourGenerator = () => {
+  // create variables to store colour values
+  const colourValues = [
+    { value: null, colour: "red" },
+    { value: null, colour: "green" },
+    { value: null, colour: "blue" },
+  ];
+
+  const assignedValues = colourValues.map((colour) => {
+    const randomNumber = Math.floor(Math.random() * 256);
+    return randomNumber;
+  });
+
+  return assignedValues;
+};
+
+// for each colour block randomly generate 3 numbers between 0 and 255
+
+// push the colour to the array
+
+// function to choose random color from array
+
+// store randomly chosen colour in a variable
+
+// grab dom element and dislpay the image to the player
+
+// on click function
+// determine of the colour clicked matches the colour that is requested
+
+// function to determine the maximum length of the array (3 or 6)
+
+// function to generate new colours
+
+const init = () => {
+  rgbGame.colourGenerator();
+};
+
+init();
