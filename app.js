ab6097462e297b9d6b105e28cd3965fb37769129
David Downie <daviddownie@gmail.com>
2020-08-07 13:46:39 -0400

    assigned randomly generated numbers to individual colour arrays

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const rgbGame = {};
 
 // grab elements from the dom
+rgbGame.easyButton = document.querySelector(".easy");
+rgbGame.hardButton = document.querySelector(".hard");
 
 // initialize array to store colors
-rgbGame.colourArray = [];
+rgbGame.colourArray = [
+  { id: 1, colour: null },
+  { id: 2, colour: null },
+  { id: 3, colour: null },
+  { id: 4, colour: null },
+  { id: 5, colour: null },
+  { id: 6, colour: null },
+];
+rgbGame.card;
 
 // function to determine colours
 rgbGame.colourGenerator = () => {
@@ -22,8 +32,19 @@ rgbGame.colourGenerator = () => {
   return assignedValues;
 };
 
-// for each colour block randomly generate 3 numbers between 0 and 255
+rgbGame.colourCardTemplate = () => {
+  rgbGame.card = `<div></div>`;
+  return rgbGame.card;
+};
 
+// for each colour block randomly generate 3 numbers between 0 and 255
+rgbGame.assignColours = () => {
+  console.log(rgbGame.colourArray);
+  rgbGame.colourArray.forEach((colour) => {
+    const newColour = rgbGame.colourGenerator();
+    colour.colour = newColour;
+  });
+};
 // push the colour to the array
 
 // function to choose random color from array
@@ -36,11 +57,21 @@ rgbGame.colourGenerator = () => {
 // determine of the colour clicked matches the colour that is requested
 
 // function to determine the maximum length of the array (3 or 6)
+// rgbGame.difficulty = () => {
+//   document.addEventListener("click", () => {
+//     if (event.target.matches(".easy")) {
+//     } else if (event.target.matches(".hard")) {
+//     }
+//   });
+// };
 
 // function to generate new colours
 
 const init = () => {
   rgbGame.colourGenerator();
+  //   rgbGame.difficulty();
+  rgbGame.assignColours();
+  console.log(rgbGame.colourArray);
 };
 
 init();
